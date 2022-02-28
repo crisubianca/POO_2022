39e32dd3f39b351b5bb383d0d79ac9351d1da87c
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-02-28 17:02:28 +0200

    Create Functions.h

---
+++ Laborator2/Ex2/Functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Student.h"
+short CompareNames(Student n1, Student n2);
+short CompareGradeM(Student n1, Student n2);
+short CompareGradeE(Student n1, Student n2);
+short CompareGradeH(Student n1, Student n2);
+short CompareAverage(Student n1, Student n2);

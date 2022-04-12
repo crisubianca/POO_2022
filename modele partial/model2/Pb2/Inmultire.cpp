81588d0beb30c991b6a30b427f6586f7e7fcf8ab
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-12 08:40:34 +0300

    Create Inmultire.cpp

---
+++ modele partial/model2/Pb2/Inmultire.cpp
@@ -0,0 +1,13 @@
+#include "Inmultire.h"
+int Inmultire::Calcul(int a, int b)
+{
+	return (a * b);
+}
+int Inmultire::GetOperationType()
+{
+	return 2;
+}
+const char* Inmultire::GetName()
+{
+	return "Inmultire";
+}

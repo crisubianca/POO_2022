eaf964fa523fe2f1da77bdb6504e6737ca9d375a
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-12 08:40:40 +0300

    Create Impartire.cpp

---
+++ modele partial/model2/Pb2/Impartire.cpp
@@ -0,0 +1,14 @@
+#include "Impartire.h"
+
+int Impartire::Calcul(int a, int b)
+{
+	return (a / b);
+}
+int Impartire::GetOperationType()
+{
+	return 3;
+}
+const char* Impartire::GetName()
+{
+	return "Impartire";
+}

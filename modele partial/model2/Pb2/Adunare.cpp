8bd7b3f78eb0b1f342a44f7572c20ef3abc97181
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-12 08:40:12 +0300

    Create Adunare.cpp

---
+++ modele partial/model2/Pb2/Adunare.cpp
@@ -0,0 +1,13 @@
+#include "Adunare.h"
+int Adunare::Calcul(int a, int b)
+{
+	return (a + b);
+}
+int Adunare::GetOperationType()
+{
+	return 0;
+}
+const char* Adunare::GetName()
+{
+	return "Adunare";
+}
